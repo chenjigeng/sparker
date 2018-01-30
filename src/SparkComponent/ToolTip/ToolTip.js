3dde6ea9e59ec0ad054435966072e1082461fd4e
chenjigeng <178854407@qq.com>
2018-01-31 01:24:05 +0800

    feat:完成tooltips组件的基本功能

--- src/SparkComponent/ToolTip/ToolTip.js
+++ src/SparkComponent/ToolTip/ToolTip.js
@@ -2,16 +2,31 @@ import React from 'react';
 import './ToolTip.less';
 
 export class ToolTip extends React.Component {
+
+  updateToolTip = () => {
+    const clientRect = this.content.getClientRects()[0];
+    const popupLeft = clientRect.x - this.popup.clientWidth / 2 + clientRect.width / 2;
+    const popupTop = clientRect.y + clientRect.height + 10;
+    this.popup.style.left = `${popupLeft}px`;
+    this.popup.style.top = `${popupTop}px`;
+  }
+  
   
   render () {
     const { children, content } = this.props; 
     return  (
-      <div className="spark-tooltip">
-        <div className="spark-tooltip__content">
+      <div className="spark-tooltip" onMouseOver={this.updateToolTip}>
+        <div 
+          className="spark-tooltip__content"
+          ref={(content) => this.content = content}
+        >
           { children }
         </div>
-        <div className="spark-tooltip__popup">
-          { content }
+        <div 
+          className="spark-tooltip__popup"
+          ref={(popup) => this.popup = popup}
+        >
+          <div>{ content }</div>
         </div>
       </div>
     );
