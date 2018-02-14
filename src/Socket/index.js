ff5a5c5a556ab3271f3a55a9a178e58d6600f05f
chenjigeng <178854407@qq.com>
2018-02-14 19:00:30 +0800

    feat:根据开发/和发布的版本选择不同的sockerServer

--- src/Socket/index.js
+++ src/Socket/index.js
@@ -1,7 +1,9 @@
 import io from 'socket.io-client';
 
+const sockerServer = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3001': 'http://193.112.17.138:3001';
+
 const socketInit = function() {
-  return io('http://127.0.0.1:3001');
+  return io(sockerServer);
 };
 
 export { socketInit };
