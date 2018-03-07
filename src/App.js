a0144a49320649a0eda9a9142343a8029ad0b37b
chenjigeng <178854407@qq.com>
2018-03-07 13:49:10 +0800

    feat:将fontawesome字体作为内部引用

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { SparkerEditor } from './Component/SparkerEditor';
 import { Router } from './router';
 import store from './redux';
+import './Common/fontawesome.css';
 
 class App extends Component {
   render() {
